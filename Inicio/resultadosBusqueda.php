b441d75502ab0535d85e83dd357f6228736a8ac7
Andreco <320266241@alumno.enp.unam.mx>
2021-05-12 13:02:59 -0500

    FEAT: integre sql al buscador

--- Inicio/resultadosBusqueda.php
+++ Inicio/resultadosBusqueda.php
@@ -1,41 +1,74 @@
-<?php
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Descripción del libro</title>
-    </head>
-    <body>
-        <h1>Tu resultado:</h1> 
-        <table border="1" cellpadding=25>
-            <tbody>
-                <tr>';
-                    for ($i=0; $i<6 ; $i++) {                     
-                        echo '<td>
-                            <img src="./Hombre san Petesburgo.jpg" alt="Hombre San Petesburgo" style="width: 400px;">
-                            <hr><hr>
-                            <ul>
-                                <li>Nombre: El hombre de San Petesburgo</li>
-                                <li>Editorial: DeBolisllo</li>
-                                <li>Autor: Ken Follet</li>
-                            </ul>
-                            <form action="./detailbook.php" method="POST">
-                                <button type="submit" name="">Seleccionar libro</button>
-                            </form>
-                            <br>
-                            <form action="./Descripción libro.php" method="POST">
-                                <button type="submit" name="">Marcar como favorito</button>
-                            </form>
-                            <br>
-                            <a href="" name="Hola">Abrir en otra ventana</a><br><br>
-                            <a href="" name="Hola">Descargar el archivo</a>
-                        </td>';
-                    }
-                echo '</tr>
-            </tbody>
-        </table>
-    </body>
-    </html>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include("config.php");
+        $filtro = $_POST["filtro"];
+        $keyword = $_POST["Buscador"];
+
+        $conexion=connect();
+        if($filtro == 'G')//G Y E A
+        {
+            $busqueda = 'genero';
+        }
+        elseif($filtro == 'Y')
+        {
+            $busqueda = 'año';
+        }
+        elseif($filtro == 'E')
+        {
+            $busqueda = 'editorial';
+        }
+        elseif($filtro == 'A')
+        {
+            $busqueda = 'autor';
+        }
+        if($filtro == 'G')
+        {
+            $peticion = "SELECT libro.id_libro, titulo, linkImagen FROM librohasgenero
+                        INNER JOIN libro ON libro.id_libro = librohasgenero.id_libro
+                        INNER JOIN genero ON genero.id_genero = librohasgenero.id_genero
+                        WHERE genero LIKE '$keyword'";
+        }
+        elseif($filtro == 'E' || $filtro == 'A')
+        {
+            $peticion = "SELECT libro.id_libro, titulo, linkImagen FROM libro
+                        INNER JOIN $busqueda ON libro.id_$busqueda = $busqueda.id_$busqueda
+                        WHERE $busqueda LIKE '$keyword'";
+        }
+        else
+        {
+            $peticion = "SELECT id_libro, titulo, linkImagen FROM libro
+                        WHERE año = $keyword";
+        }
+        $query = mysqli_query($conexion,$peticion);
+        echo "<table border=1>";
+            echo "<tbody>";
+                echo "<tr>";
+                while($row=mysqli_fetch_array($query))
+                {
+                    echo "<td>";
+                        echo "<img src='$row[linkImagen]'>";
+                        echo "<br>";
+                        echo "nombre: $row[titulo]";
+                        echo "<br>";
+                        echo "id del libro: $row[id_libro]";
+                        echo "<br>";
+                        echo "<form action='./detailbook.php' method='POST'>";
+                            echo "<input type='hidden' name='id_libro' value='$row[id_libro]'>";
+                            echo "<button type='submit'>Ver libro</button>";
+                        echo "</form>";
+                    echo "</td>";
+                }
+                echo "</tr>";
+            echo "</tbody>";
+        echo "</table>";
+    ?>
+</body>
+</html>
